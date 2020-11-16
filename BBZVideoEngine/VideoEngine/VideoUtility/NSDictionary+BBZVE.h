03d084f427e31c39b65e8efacceceb2981d22dcd
HBO <haibo.zhu@shopee.com>
2020-11-16 18:54:55 +0800

    去除YYKIT依赖

---
+++ BBZVideoEngine/VideoEngine/VideoUtility/NSDictionary+BBZVE.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+BBZVE.h
+//  BBZVideoEngine
+//
+//  Created by Hbo on 2020/11/16.
+//  Copyright © 2020 HaiboZhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (BBZVE)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BBZVideoEngine/VideoEngine/VideoUtility/NSDictionary+BBZVE.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+BBZVE.m
+//  BBZVideoEngine
+//
+//  Created by Hbo on 2020/11/16.
+//  Copyright © 2020 HaiboZhu. All rights reserved.
+//
+
+#import "NSDictionary+BBZVE.h"
+
+@implementation NSDictionary (BBZVE)
+
+@end
